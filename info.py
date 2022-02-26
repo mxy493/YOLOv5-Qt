dc180c818762796cea6d9554d22baa57a42538e6
mxy <mxy493@qq.com>
2022-02-26 14:21:55 +0800

    更新v1.5.0版
    Signed-off-by: mxy <mxy493@qq.com>

--- info.py
+++ info.py
@@ -9,5 +9,5 @@ Desc: 一些关于程序的信息
 
 APP_NAME = 'YOLOv5-Qt'
 APP_TITLE = 'YOLOv5 Object Detection'
-APP_VERSION = '1.4.0'
-RECENT_UPDATE = '2021-10-25'
+APP_VERSION = '1.5.0'
+RECENT_UPDATE = '2021-02-26'
